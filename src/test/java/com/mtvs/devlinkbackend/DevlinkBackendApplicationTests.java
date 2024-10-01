a6e579223c399506b8f4d3a8331667b31c30b11c
lsy1307 <lsy1307@inha.edu>
2024-10-01 17:29:14 +0900

    Initial commit

---
+++ src/test/java/com/mtvs/devlinkbackend/DevlinkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtvs.devlinkbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevlinkBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
