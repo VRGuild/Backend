24140f33d535b2fa724c76556688f462b1e54dc7
in seong Park <123macanic@naver.com>
2024-10-21 14:27:01 +0900

    refactor: 도메인 네임 변경 request -> project
    변경된 점 :
    - Repository 메서드 수정

--- src/test/java/com/mtvs/devlinkbackend/crud/ProjectCRUDTest.java
+++ src/test/java/com/mtvs/devlinkbackend/crud/ProjectCRUDTest.java
@@ -174,7 +174,7 @@ public class ProjectCRUDTest {
     @Order(2)
     public void testFindRequestsByTitleContainingIgnoreCase(String title) {
         Assertions.assertDoesNotThrow(() ->
-                System.out.println("Request = " + projectService.findRequestsByTitleContainingIgnoreCase(title)));
+                System.out.println("Request = " + projectService.findProjectsByTitleContainingIgnoreCase(title)));
     }
 
     @DisplayName("필요 직군보다 더 많이 모집하는 의뢰 조회 테스트")
