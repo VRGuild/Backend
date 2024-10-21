040abd287f18c907cdcf2c1f1b1e1da834ae5a92
in seong Park <123macanic@naver.com>
2024-10-21 14:26:32 +0900

    feat: 프로젝트 지원 CRD API 구현
    추가된 기능:
    - Entity
    - Repository
    - Service
    - DTO
    - Controller

---
+++ src/main/java/com/mtvs/devlinkbackend/support/service/SupportService.java
@@ -0,0 +1,37 @@
+package com.mtvs.devlinkbackend.support.service;
+
+import com.mtvs.devlinkbackend.support.dto.SupportRegistRequestDTO;
+import com.mtvs.devlinkbackend.support.entity.Support;
+import com.mtvs.devlinkbackend.support.repository.SupportRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SupportService {
+    private final SupportRepository supportRepository;
+
+    public SupportService(SupportRepository supportRepository) {
+        this.supportRepository = supportRepository;
+    }
+
+    public Support createSupport(SupportRegistRequestDTO supportRegistRequestDTO) {
+        return supportRepository.save(new Support(
+                supportRegistRequestDTO.getProjectId(),
+                supportRegistRequestDTO.getTeamId(),
+                "waiting"
+        ));
+    }
+
+    public List<Support> findSupportsByProjectId(Long projectId) {
+        return supportRepository.findSupportsByProjectId(projectId);
+    }
+
+    public List<Support> findSupportsByTeamId(Long teamId) {
+        return supportRepository.findSupportsByTeamId(teamId);
+    }
+
+    public void deleteSupport(Long supportId) {
+        supportRepository.deleteById(supportId);
+    }
+}
