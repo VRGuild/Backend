dd18d0ca96bafb0ea3ae7c31b2e71bd8e89a5b96
lsy1307 <lsy1307@inha.edu>
2024-11-06 11:12:59 +0900

    Test: documentdb연결 테스트

--- src/main/java/com/mtvs/devlinkbackend/DevlinkBackendApplication.java
+++ src/main/java/com/mtvs/devlinkbackend/DevlinkBackendApplication.java
@@ -1,5 +1,10 @@
 package com.mtvs.devlinkbackend;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
@@ -11,5 +16,4 @@ public class DevlinkBackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(DevlinkBackendApplication.class, args);
     }
-
 }
