da08a76b8a780880604b59ac504bed9fc36d98a0
in seong Park <123macanic@naver.com>
2024-11-06 22:06:48 +0900

    fix : 이전의 Entity로 만들어진 JPQL 메서드 삭제

--- src/main/java/com/mtvs/devlinkbackend/project/repository/ProjectViewRepository.java
+++ src/main/java/com/mtvs/devlinkbackend/project/repository/ProjectViewRepository.java
@@ -2,19 +2,15 @@ package com.mtvs.devlinkbackend.project.repository;
 
 import com.mtvs.devlinkbackend.project.entity.Project;
 import com.mtvs.devlinkbackend.project.repository.projection.ProjectIdAndContent;
-import com.mtvs.devlinkbackend.project.repository.projection.ProjectSummary;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
-import java.util.List;
 
 public interface ProjectViewRepository extends JpaRepository<Project, Long> {
     // AccountId로 프로젝트 조회 (페이징 추가)
-    Page<Project> findProjectsByAccountId(String accountId, Pageable pageable);
+    Page<Project> findProjectsByUserId(Long userId, Pageable pageable);
 
     // WorkType으로 프로젝트 조회 (페이징 추가)
     Page<Project> findProjectsByWorkType(String workType, Pageable pageable);
@@ -26,24 +22,9 @@ public interface ProjectViewRepository extends JpaRepository<Project, Long> {
     Page<Project> findProjectsByTitleContainingIgnoreCase(String title, Pageable pageable);
 
     // 시작일과 종료일 기준으로 프로젝트 조회 (페이징 추가)
-    Page<Project> findProjectsByStartDateTimeLessThanEqualOrEndDateTimeGreaterThanEqual(
-            LocalDate startDateTime, LocalDate endDateTime, Pageable pageable);
+    Page<Project> findProjectsByStartDateLessThanEqualOrEndDateGreaterThanEqual(
+            LocalDate startDate, LocalDate endDate, Pageable pageable);
 
-    // required 값들이 넘겨준 값보다 큰 row를 조회하는 쿼리 (페이징 추가)
-    @Query("SELECT p FROM Project p WHERE " +
-            "(:requiredClient IS NULL OR p.requiredClient > :requiredClient) AND " +
-            "(:requiredServer IS NULL OR p.requiredServer > :requiredServer) AND " +
-            "(:requiredDesign IS NULL OR p.requiredDesign > :requiredDesign) AND " +
-            "(:requiredPlanner IS NULL OR p.requiredPlanner > :requiredPlanner) AND " +
-            "(:requiredAIEngineer IS NULL OR p.requiredAIEngineer > :requiredAIEngineer)")
-    Page<Project> findProjectsWithLargerRequirements(
-            @Param("requiredClient") Integer requiredClient,
-            @Param("requiredServer") Integer requiredServer,
-            @Param("requiredDesign") Integer requiredDesign,
-            @Param("requiredPlanner") Integer requiredPlanner,
-            @Param("requiredAIEngineer") Integer requiredAIEngineer,
-            Pageable pageable
-    );
 
     ProjectIdAndContent findProjectIdAndContentByProjectId(Long projectId);
 
