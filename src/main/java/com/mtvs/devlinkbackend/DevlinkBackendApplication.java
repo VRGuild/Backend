0baaad1ba3e34dc2035e9b957434096cc124c3cc
lsy1307 <lsy1307@inha.edu>
2024-11-06 11:12:59 +0900

    Test: documentdb연결 테스트

--- src/main/java/com/mtvs/devlinkbackend/DevlinkBackendApplication.java
+++ src/main/java/com/mtvs/devlinkbackend/DevlinkBackendApplication.java
@@ -16,23 +16,4 @@ public class DevlinkBackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(DevlinkBackendApplication.class, args);
     }
-    @PostConstruct
-    public void init() {
-        String template = "mongodb://%s:%s@%s/devlink?ssl=true&replicaSet=rs0&readpreference=%s";
-        String username = "ebroot";
-        String password = "qazwsx1357*";
-        String clusterEndpoint = "docdb-2024-11-01-16-21-04.cluster-cpiycgkkk147.ap-northeast-2.docdb.amazonaws.com";
-        String readPreference = "secondaryPreferred";
-        String connectionString = String.format(template, username, password, clusterEndpoint, readPreference);
-
-        String truststore = "classpath:rds-truststore.jks";
-        String truststorePassword = "qazwsx1357*";
-
-        System.setProperty("javax.net.ssl.trustStore", truststore);
-        System.setProperty("javax.net.ssl.trustStorePassword", truststorePassword);
-
-        MongoClient mongoClient = MongoClients.create(connectionString);
-
-        MongoDatabase testDB = mongoClient.getDatabase("devlink");
-    }
 }
