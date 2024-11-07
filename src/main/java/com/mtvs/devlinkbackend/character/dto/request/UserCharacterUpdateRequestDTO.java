a4cbe9e251c6dba65280587bbff9df798ce830fd
in seong Park <123macanic@naver.com>
2024-11-07 09:39:47 +0900

    refactor: 기존의 정해지지 않은 컬럼 CharacterPicture에 대한 타입 구체화

--- src/main/java/com/mtvs/devlinkbackend/character/dto/request/UserCharacterUpdateRequestDTO.java
+++ src/main/java/com/mtvs/devlinkbackend/character/dto/request/UserCharacterUpdateRequestDTO.java
@@ -1,5 +1,6 @@
 package com.mtvs.devlinkbackend.character.dto.request;
 
+import com.mtvs.devlinkbackend.character.entity.CharacterPicture;
 import com.mtvs.devlinkbackend.character.entity.CustomInfo;
 import lombok.*;
 
@@ -14,5 +15,5 @@ public class UserCharacterUpdateRequestDTO {
     private Long guildId;
     private List<Long> teamIdList;
     private List<CustomInfo> customList;
-    private String characterPicture;
+    private CharacterPicture characterPicture;
 }
