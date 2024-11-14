626e2c65400ef86f350f1dddb380fe2225711db0
in seong Park <123macanic@naver.com>
2024-11-14 11:29:14 +0900

    refactor: member 오류 수정

--- src/main/java/com/mtvs/devlinkbackend/member/query/controller/MemberQueryController.java
+++ src/main/java/com/mtvs/devlinkbackend/member/query/controller/MemberQueryController.java
@@ -6,10 +6,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/member")
@@ -26,7 +23,7 @@ public class MemberQueryController {
             @ApiResponse(responseCode = "200", description = "성공적으로 조회되었습니다."),
             @ApiResponse(responseCode = "404", description = "팀을 찾을 수 없습니다.")
     })
-    @PatchMapping("/{memberId}")
+    @GetMapping("/{memberId}")
     public ResponseEntity<MemberStatusResponseDTO> updateTeam(
             @PathVariable(name = "memberId") Long memberId) {
 
