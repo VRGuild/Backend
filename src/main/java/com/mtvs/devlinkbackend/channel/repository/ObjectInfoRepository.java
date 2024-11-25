a827edfb6b4812d57dfd5d3f0d2eef05b4880085
in seong Park <123macanic@naver.com>
2024-11-25 16:37:31 +0900

    feat: ObjectInfo 도메인 CRUD API 생성 및 TestCode 작성

---
+++ src/main/java/com/mtvs/devlinkbackend/channel/repository/ObjectInfoRepository.java
@@ -0,0 +1,9 @@
+package com.mtvs.devlinkbackend.channel.repository;
+
+import com.mtvs.devlinkbackend.channel.entity.ObjectInfo;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ObjectInfoRepository extends MongoRepository<ObjectInfo, String> {
+}
