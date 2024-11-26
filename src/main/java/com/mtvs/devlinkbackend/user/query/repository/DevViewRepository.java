c98c9afc6e1dc88a82cc8bbda3ac52c25fd37a2e
in seong Park <123macanic@naver.com>
2024-11-26 14:35:58 +0900

    refactor: 평가 완료되지 않은 Dev들만 보내는 로직으로 변경

--- src/main/java/com/mtvs/devlinkbackend/user/query/repository/DevViewRepository.java
+++ src/main/java/com/mtvs/devlinkbackend/user/query/repository/DevViewRepository.java
@@ -4,6 +4,7 @@ import com.mtvs.devlinkbackend.user.command.model.entity.Dev;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -19,4 +20,11 @@ public interface DevViewRepository extends JpaRepository<Dev, Long> {
     List<Dev> findDevsByDevEmail(String devEmail);
 
     Page<Dev> findAllBy(Pageable pageable);
+
+    @Query("""
+           SELECT d FROM Dev d WHERE EXISTS (
+               SELECT sc FROM SkillCategoryInfo sc WHERE sc.dev = d AND sc.isEvaluated = false
+           )
+           """)
+    Page<Dev> findAllWithUnevaluatedSkillCategory(Pageable pageable);
 }
