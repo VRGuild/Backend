0724223798ffe5eb1697414d22dfc66b4b622f98
Yeonri <lsy1307@inha.edu>
2024-11-27 11:16:24 +0900

    Fix: Page import구문 수정

--- src/main/java/com/mtvs/devlinkbackend/channel/repository/ChannelViewRepository.java
+++ src/main/java/com/mtvs/devlinkbackend/channel/repository/ChannelViewRepository.java
@@ -2,10 +2,10 @@ package com.mtvs.devlinkbackend.channel.repository;
 
 import com.mtvs.devlinkbackend.channel.entity.Channel;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.awt.print.Pageable;
 
 @Repository
 public interface ChannelViewRepository extends MongoRepository<Channel, String> {
